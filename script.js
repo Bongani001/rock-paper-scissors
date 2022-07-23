244ae40186921603b2f079e6865d45de868544eb
Bongani001 <bbongs421@gmail.com>
2022-07-23 17:18:01 +0200

    Add functions

--- script.js
+++ script.js
@@ -11,28 +11,61 @@ function getComputerChoice() {
     }
 }
 
+let playerGuess = 'rock'
+let playerSelect = playerGuess.toLowerCase()
+let playerSelection = playerSelect[0].toUpperCase() + playerSelect.slice(1);
+
+let playerWin = 0;
+let computerWin = 0;
 function playRound(playerSelection, computerSelection) {
+    
     if (playerSelection == computerSelection) {
         let result = `It's A Tie!`;
+        playerWin++;
+        computerWin++;
         return result;
     } else if (playerSelection == 'Rock' && computerSelection == 'Paper') {
+        computerWin++;
         return `You Lose! ${computerSelection} beats ${playerSelection}`;
     } else if (playerSelection == 'Rock' && computerSelection == 'Scissors') {
+        playerWin++;
         return `You Win! ${playerSelection} beats ${computerSelection}`;
     } else if (playerSelection == 'Paper' && computerSelection == 'Scissors') {
+        computerWin++;
         return `You Lose! ${computerSelection} beats ${playerSelection}`;
     } else if (playerSelection == 'Paper' && computerSelection == 'Rock') {
+        playerWin++;
         return `You Win! ${playerSelection} beats ${computerSelection}`;
     } else if (playerSelection == 'Scissors' && computerSelection == 'Rock') {
+        computerWin++;
         return `You Lose! ${computerSelection} beats ${playerSelection}`;
     } else {
         return `You Win! ${playerSelection} beats ${computerSelection}`;
     }
 }
 
-let playerGuess = 'rock'
-let playerSelect = playerGuess.toLowerCase()
-let playerSelection = playerSelect[0].toUpperCase() + playerSelect.slice(1);
-let computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
 
+let computerSelection = getComputerChoice();
+
+let i = 0;
+function game() {
+    let round = playRound(playerSelection, computerSelection);
+    console.log(round,` Player Wins: ${playerWin}`, `Computer Wins: ${computerWin}`);
+    i++;
+}
+
+function winner() {
+    if (playerWin > computerWin) {
+        console.log('You win the game, well done.');
+    } else if (playerWin < computerWin) {
+        console.log('You lost the game, better luck next time.');
+    } else {
+        console.log('It\'s a draw.')
+    }
+}
+
+for (let i = 0; i < 5; i++) {
+    console.log(game());
+}
+
+console.log(winner())
